4bda9e0ece156058b6d50adc2fd542174b5911e7
Joppe Aarts <joppe@zicht.nl>
2015-04-24 14:34:13 +0000

    install script modified copy zicht_htmldev.yml copy bower.json
    git-svn-id: svn://zon/projecten/zicht-libraries/ZichtHtmldevBundle/trunk@61312 0f35748b-8228-47f0-bbea-889afbbd9a42

--- src/Zicht/Bundle/HtmldevBundle/bin/install.sh
+++ src/Zicht/Bundle/HtmldevBundle/bin/install.sh
@@ -5,4 +5,6 @@ ROOT=$(pwd)
 mkdir -p htmldev/{images,style,sass,javascript}
 
 cp vendor/zicht/htmldev-bundle/src/Zicht/Bundle/HtmldevBundle/Resources/.bowerrc htmldev/.bowerrc
+cp vendor/zicht/htmldev-bundle/src/Zicht/Bundle/HtmldevBundle/Resources/bower.json htmldev/bower.json
 cp vendor/zicht/htmldev-bundle/src/Zicht/Bundle/HtmldevBundle/Resources/views/_index.html.twig htmldev/_index.html.twig
+cp vendor/zicht/htmldev-bundle/src/Zicht/Bundle/HtmldevBundle/Resources/config/zicht_htmldev.yml app/config/bundles/zicht_htmldev.yml
