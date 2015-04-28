f1c26ec14621f27b0c956a809371ca9b78188291
Joppe Aarts <joppe@zicht.nl>
2015-04-28 07:09:51 +0000

    phpdoc
    git-svn-id: svn://zon/projecten/zicht-libraries/ZichtHtmldevBundle/trunk@61324 0f35748b-8228-47f0-bbea-889afbbd9a42

--- src/Zicht/Bundle/HtmldevBundle/ZichtHtmldevBundle.php
+++ src/Zicht/Bundle/HtmldevBundle/ZichtHtmldevBundle.php
@@ -1,9 +1,16 @@
 <?php
-
+/**
+ * @copyright Zicht online <http://zicht.nl>
+ */
 namespace Zicht\Bundle\HtmldevBundle;
 
 use \Symfony\Component\HttpKernel\Bundle\Bundle;
 
+/**
+ * Class ZichtHtmldevBundle
+ *
+ * @package Zicht\Bundle\HtmldevBundle
+ */
 class ZichtHtmldevBundle extends Bundle
 {
 }
