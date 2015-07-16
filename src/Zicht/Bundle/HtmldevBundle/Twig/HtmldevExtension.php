c369d3ea5c528f66646975a87a56f6a413628fca
Boudewijn Schoon <boudewijn@zicht.nl>
2015-07-16 09:50:39 +0000

    adds get_current_datetime function
    git-svn-id: svn://zon/projecten/zicht-libraries/ZichtHtmldevBundle/trunk@65637 0f35748b-8228-47f0-bbea-889afbbd9a42

--- src/Zicht/Bundle/HtmldevBundle/Twig/HtmldevExtension.php
+++ src/Zicht/Bundle/HtmldevBundle/Twig/HtmldevExtension.php
@@ -18,8 +18,45 @@ class HtmldevExtension extends Twig_Extension
      */
     function getFunctions() {
         return array(
-            'ui_and_html' => new \Twig_Function_Method($this, 'getUiAndHtml', array('is_safe' => array('html')))
+            new \Twig_SimpleFunction('ui_and_html', array($this, 'ui_and_html'), array('is_safe' => array('html'))),
+            new \Twig_SimpleFunction('get_ui_and_html', array($this, 'get_ui_and_html'), array('is_safe' => array('html'))),
+            new \Twig_SimpleFunction('get_current_datetime', array($this, 'get_current_datetime')),
         );
+    }
+
+    /**
+     * Returns the current time with, optionally, a modifier
+     *
+     * > get_current_datetime()
+     * Datetime('now')
+     *
+     * > get_current_datetime('+1 day')
+     * Datetime('now')->modify('+1 day')
+     *
+     * @param string $modify
+     * @return \DateTime
+     */
+    public function get_current_datetime($modify = '')
+    {
+        // use static $now to ensure that all dates are -exactly- the same
+        static $now = null;
+        if (null === $now) { $now = new \DateTime('now'); }
+
+        $datetime = clone $now;
+        if (!empty($modify)) {
+            $datetime->modify($modify);
+        }
+
+        return $datetime;
+    }
+
+    /**
+     * @param $html
+     * @deprecated Use get_ui_and_html instead
+     */
+    public function ui_and_html($html)
+    {
+        return $this->get_ui_and_html($html);
     }
 
     /**
@@ -28,7 +65,7 @@ class HtmldevExtension extends Twig_Extension
      * @param $html
      * @return string
      */
-    public function getUiAndHtml($html)
+    public function get_ui_and_html($html)
     {
         $resultHtml = sprintf('%s
             <pre>
