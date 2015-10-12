077f71e177696b2b2fe6c5d6a188b78f968f9db9
Robert van Kempen <robert@zicht.nl>
2015-10-12 13:03:54 +0000

    Add component macro to htmldev, and update base.html to modern times
    git-svn-id: svn://zon/projecten/zicht-libraries/ZichtHtmldevBundle/trunk@70624 0f35748b-8228-47f0-bbea-889afbbd9a42

--- bin/htmldev.sh
+++ bin/htmldev.sh
@@ -6,9 +6,15 @@ RESOURCES=vendor/zicht/htmldev-bundle/src/Zicht/Bundle/HtmldevBundle/Resources
 CONFIG=app/config/bundles
 
 if [ ! -d $HTMLDEV ]; then
-    mkdir -p $HTMLDEV/{images,style,sass,javascript}
+    mkdir -p $HTMLDEV/{images,javascript,macros,style,sass}
+    cp $RESOURCES/views/_index.html.twig $HTMLDEV/_index.html.twig
     cp $RESOURCES/views/_index.html.twig $HTMLDEV/_index.html.twig
     cp $RESOURCES/views/_base.html.twig $HTMLDEV/_base.html.twig
+    cp $RESOURCES/views/macros/components.html.twig $HTMLDEV/macros/components.html.twig
+fi
+
+if [ ! -f $HTMLDEV/browserlist ]; then
+    cp $RESOURCES/browserlist $HTMLDEV/browserlist
 fi
 
 if [ ! -f $ROOT/.bowerrc ]; then
