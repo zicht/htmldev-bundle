5a5bf65f77ab86b16ed66547c0747557e18bec48
Joppe Aarts <joppe@zicht.nl>
2015-10-28 10:20:38 +0000

    removed map property from systemjs config added comments how to add map and meta property for systemjs config
    git-svn-id: svn://zon/projecten/zicht-libraries/ZichtHtmldevBundle/branches/2.0.x@71792 0f35748b-8228-47f0-bbea-889afbbd9a42

--- src/Zicht/Bundle/HtmldevBundle/Resources/system.conf.js
+++ src/Zicht/Bundle/HtmldevBundle/Resources/system.conf.js
@@ -3,10 +3,25 @@
 System.config({
     baseURL: '/bundles/@bundle@/javascript',
 
-    transpiler: 'babel',
-
-    map: {
-        jquery: '../vendor/jquery/dist/jquery.js',
-        hamburger: '../vendor/zicht-jquery-hamburger/src/js/jquery.hamburger.js'
-    }
+    transpiler: 'babel'
 });
+
+/*
+For es5 libraries use the "map" property like
+
+map: {
+    jquery: 'vendor/jquery/dist/jquery.js',
+    underscore: 'vendor/underscore/underscore.js',
+    backbone: 'vendor/backbone/backbone.js',
+    react: 'vendor/react/react.js'
+},
+
+
+To define dependencies for es5 libraries use the "meta" property like
+
+meta: {
+    backbone: {
+        deps: ['jquery', 'underscore']
+    }
+}
+*/
