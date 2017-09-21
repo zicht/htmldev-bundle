68fa34d5b0ce7187e85d38090fa847cd46577406
Robert van Kempen <robert@zicht.nl>
2017-09-21 21:13:02 +0200

    Remove Symfony form rendering

--- src/Zicht/Bundle/HtmldevBundle/Controller/HtmldevController.php
+++ src/Zicht/Bundle/HtmldevBundle/Controller/HtmldevController.php
@@ -8,8 +8,6 @@ namespace Zicht\Bundle\HtmldevBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
-use Symfony\Component\Form\FormError;
-use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\HttpFoundation\Response;
 use \Twig_Environment;
 use Zicht\Bundle\HtmldevBundle\Service\Template as PageService;
@@ -37,24 +35,17 @@ class HtmldevController
     private $templating;
 
     /**
-     * @var FormFactoryInterface
-     */
-    private $formFactory;
-
-    /**
      * Initializes a new instance of the HtmldevController class.
      *
      * @param PageService $template
      * @param Twig_Environment $twig
      * @param EngineInterface $templating
-     * @param FormFactoryInterface $formFactory
      */
-    public function __construct(PageService $template, Twig_Environment $twig, EngineInterface $templating, FormFactoryInterface $formFactory)
+    public function __construct(PageService $template, Twig_Environment $twig, EngineInterface $templating)
     {
         $this->template = $template;
         $this->twig = $twig;
         $this->templating = $templating;
-        $this->formFactory = $formFactory;
     }
 
     /**
@@ -72,29 +63,5 @@ class HtmldevController
         return $this->templating->renderResponse(sprintf('@htmldev/%s', $this->template->find($filename)), array(
             'templates' => $this->template->findAll()
         ));
-    }
-
-    /**
-     * Renders a form using the `htmldev_example` type. Including errors.
-     *
-     * @return array
-     *
-     * @Route("/_form")
-     * @Template
-     */
-    public function formAction()
-    {
-        $form = $this->formFactory->create('htmldev_example');
-
-        $form->addError(new FormError('This is an example form error message'));
-        $form->get('htmldev_email')->addError(new FormError('This is an example form field error message'));
-        $form->get('htmldev_select_dropdown')->addError(new FormError('This is an example form field error message related to choices'));
-        $form->get('htmldev_checkbox_buttons')->addError(new FormError('This is an example form field error message related to choices'));
-        $form->get('htmldev_checkbox')->addError(new FormError('This is an error message related to a single checkbox item'));
-
-        return [
-            'form' => $form->createView(),
-            'form_url' => '#'
-        ];
     }
 }
