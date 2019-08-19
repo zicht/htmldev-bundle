12aaefe2fceb91dc67c6416c67d83e36f28f4bd1
boudewijn <boudewijn@sniper>
2019-08-19 16:33:50 +0200

    Add `validate_context` twig filter

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ An environment to create living styleguides.
 
 1. Require via composer.
 
-```
+```shell script
 composer require zicht/htmldev-bundle
 ```
 
@@ -14,7 +14,7 @@ composer require zicht/htmldev-bundle
 
 Add the following YML to your app's route configuration.
 
-```
+```yaml
 htmldev:
     resource: "@ZichtHtmldevBundle/Resources/config/routing.yml"
 ```
@@ -25,16 +25,32 @@ htmldev:
 
 To include files from the `htmldev` folder, use the `@htmldev` directive.
 
-```
+```twig
 {% extends '@htmldev/file.html.twig' %}
 ```
 
 To include files from the bundle, use the bundle direcyory:
 
-```
+```twig
 {% import '@ZichtHtmldevBundle/macros/components.html.twig' as ui %}
   
 {{ ui.component('cards/news, { title: 'Hodor' }) }}
+```
+
+To validate twig `_context` variables:
+
+```twig
+{% if app.debug %}
+    {#
+       Validate the provided template _context
+       For rules, see: https://github.com/rakit/validation#available-rules
+    #}
+    {{ _context|validate_context({
+        required_number: 'required|numeric',
+        required_boolean: 'required|in:1,0',
+        optional_string: 'in:foo,bar',
+    }) }}
+{% endif %}
 ```
 
 ## Maintainer
---
+++ src/Zicht/Bundle/HtmldevBundle/Twig/ValidateExtension.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * @copyright Zicht Online <http://zicht.nl>
+ */
+namespace Zicht\Bundle\HtmldevBundle\Twig;
+
+use Rakit\Validation\Validator;
+use Twig_Extension;
+use Twig_SimpleFilter;
+
+/**
+ * Twig extensions for validating data.
+ */
+class ValidateExtension extends Twig_Extension
+{
+    /**
+     * @{inheritDoc}
+     */
+    public function getFilters()
+    {
+        return [
+            new Twig_SimpleFilter('validate_context', [$this, 'validateContext'])
+        ];
+    }
+
+    /**
+     * @param array $context
+     * @param array $rules
+     * @throws \Twig\Error\Error
+     */
+    public function validateContext(array $context, array $rules)
+    {
+        $validator = new Validator();
+        $validation = $validator->validate($context, $rules);
+        if ($validation->fails()) {
+            throw new \Twig\Error\Error(sprintf("validate_context error (%d errors)\n%s", $validation->errors()->count(), json_encode($validation->errors()->firstOfAll())));
+        }
+    }
+
+    /**
+     * The name of this twig extension.
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return 'htmldev_twig_validate_extension';
+    }
+}
